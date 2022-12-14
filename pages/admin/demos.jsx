c3cbdc4bfe66e59603bed8e31529916e81905952
jado66 <77120464+jado66@users.noreply.github.com>
2022-12-13 23:06:53 -0700

    Added to demos page

--- pages/admin/demos.jsx
+++ pages/admin/demos.jsx
@@ -4,8 +4,11 @@ import LangaugesTable from "../../components/pageComponents/LanguagesTable";
 export default function Demos() {
   return (
     <Container noFooter>
-      <h1 className="h3 text-center mt-4">Demos</h1>
+      <h1 className="h3 text-center mt-4">Demos and Unfinished Reusable Components</h1>
       <hr className="bg-brand opacity-100"/>
+      <h2 className="h5 text-center my-3">Programming Languages Table</h2>
+      <hr className="bg-brand opacity-100 w-50 mx-auto"/>
+
       <LangaugesTable/>
 
     </Container>
