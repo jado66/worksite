6426321a57c370b4a469f7f873d3e8776cd64318
jado66 <77120464+jado66@users.noreply.github.com>
2022-12-20 16:32:38 -0700

    Fix

--- pages/high-tech/ai-&-ml.jsx
+++ pages/high-tech/ai-&-ml.jsx
@@ -5,6 +5,25 @@ import chatText from "./chatText";
 import "react-typewriter-animate/dist/Typewriter.css";
 import { useState } from "react";
 
+const chatText = [
+    `JD - Can you explain why we can't see black holes?`,
+
+    `Bot - Black holes are objects in space that are so dense and have such strong gravitational pull that nothing, not even light, can escape from them. Because light is the only way for us to see things, black holes are invisible to the naked eye and cannot be directly observed.
+
+However, scientists can indirectly detect the presence of a black hole through its effects on nearby objects. For example, a black hole can be detected by the way it causes nearby stars to move in their orbits, or by the way it bends and distorts light as it passes near the black hole.
+
+Additionally, black holes can emit radiation when matter falls into them, which can be detected by telescopes. This radiation is called Hawking radiation, named after the physicist Stephen Hawking who predicted its existence.
+
+So while we cannot directly see black holes, we can infer their presence through their effects on the things around them.`,
+
+`JD - Can you explain this to me as if I were in Kindergarten?`,
+`Bot - Sure! A black hole is a special kind of thing in space that is really, really heavy and has a really strong force that pulls things towards it. It's so heavy and has such a strong force that not even light, which is what helps us see things, can escape from it. That's why we can't see black holes.
+
+But scientists can still find out that black holes are there by looking at how they affect other things around them. For example, if there is a black hole nearby, it can make stars move in a certain way or make light bend. Scientists can also see the heat and energy that comes out of black holes when things fall into them.
+
+So even though we can't see black holes, we can still learn about them and know they are there!`
+]
+
 export default function BlockchainAndCrypto() {
     
     const [chatStep, setChatStep] = useState(0)
